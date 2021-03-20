0988afd480397ce11222096a4a63f124f52f3eb8
satishreddy406 <satish06406@gmail.com>
2021-03-20 11:18:40 +0530

    Hello world program added

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * Hello World Program
+		* @authorreddyl
+		* @version 1.0
+		*
+*/
+public class HelloWorld {
+	
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Hello World1");
+		//System.out.println("Hello World2");
+		System.out.println("Hello World3");
+		/*System.out.println("Hello World4");
+		System.out.println("Hello World5");*/
+		
+		
+	}
+}
+
