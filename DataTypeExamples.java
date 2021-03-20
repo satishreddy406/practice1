2e6a54af13ce2f3fb88008b4b5054574369360ba
satishreddy406 <satish06406@gmail.com>
2021-03-20 11:21:42 +0530

    Datatypes

---
+++ DataTypeExamples.java
@@ -0,0 +1,44 @@
+
+/**
+ * DataTypeExamples program
+*/
+
+public class DataTypeExamples {
+	//Integer datatypes
+		static byte value1 ;
+		static short value2;
+		static int value3 ;
+		static long value4;
+		
+		//Float Data type
+		static float f;
+		
+		//Double
+		static double d;
+		
+		//Character data type
+		static char ch;
+		
+		//String Datatype
+		static String str;		
+		
+		//Boolean Data type
+		static boolean x;
+	
+
+	public static void main(String args[]) {
+		
+		System.out.println(value1);
+				System.out.println(value2);
+		System.out.println(value3);
+				System.out.println(value4);
+								System.out.println(f);
+				System.out.println(d);
+				System.out.println(ch);
+				System.out.println(str);
+								System.out.println(x);
+				
+
+	}
+	
+}
